b0f979ebd38fcd4969ed9d42cb77f872db5fec4c
mwarimuinyasio-byte <mwarimuinyasio@gmail.com>
2026-09-03 17:10:57 +0300

    added changes

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-import pandas as pd
-import streamlit as st
-import plotly.express as px
-
 # ---------------------------------------------------
 # Page Configuration
 # ---------------------------------------------------
